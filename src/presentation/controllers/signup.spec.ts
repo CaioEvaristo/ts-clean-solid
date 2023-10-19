cfc6fcf30f5c615a6d950d8486bd72b3e8cd13fc
Caio Souza <caio.souza@hotmail.com>
2023-10-19 17:42:57 -0300

    test: create new controller to signup

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,19 @@
+import exp from 'constants';
+import { SignUpController } from './signup'
+
+describe ('SignUp Controller', () => {
+    test('Should return 400 if no name is provided', () => {
+        const sut = new SignUpController(); // system under test
+        const httpRequest = {
+            body: {
+                email: 'any_email@mail.com',
+                password: 'any_password',
+                passwordConfirmation: 'any_password'
+            }
+        }
+
+        const httpResponse = sut.handle(httpRequest);
+
+        expect(httpResponse.statusCode).toBe(400);
+    })
+})
