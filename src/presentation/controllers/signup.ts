789c539755058a51bb3cedeb5d8b228a7d8e86c8
Caio Souza <caio.souza@hotmail.com>
2023-10-19 20:06:37 -0300

    feat: ensure controller return invalid email

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -3,6 +3,7 @@ import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
 import { EmailValidator } from '../protocols/email-validator'
+import { InvalidParamError } from '../errors/invalid-param-error'
 
 export class SignUpController implements Controller{
     private readonly emailValidator: EmailValidator
@@ -18,6 +19,12 @@ export class SignUpController implements Controller{
             if (!httpRequest.body[field]) {
                 return badRequest(new MissingParamError(field))
             }
+        }
+
+        const isValid = this.emailValidator.isValid(httpRequest.body.email)
+
+        if (!isValid) {
+            return badRequest(new InvalidParamError('email'))
         }
     }
 }
