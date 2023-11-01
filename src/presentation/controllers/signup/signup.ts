661a2f5422ca591e313426cbb8f94d314b60e03c
Caio Souza <caio.souza@hotmail.com>
2023-10-31 23:44:22 -0300

    feat: ensure Add Account return 200 and tests

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -32,11 +32,16 @@ export class SignUpController implements Controller{
                 return badRequest(new InvalidParamError('email'))
             }
 
-            this.addAccount.add({
+            const account =  this.addAccount.add({
                 name: httpRequest.body.name,
                 email: httpRequest.body.email,
                 password: httpRequest.body.password
             })
+
+            return {
+                statusCode: 200,
+                body: account
+            }
 
         } catch (error) {
             return serverError()
